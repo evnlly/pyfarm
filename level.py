e27dd2aec58670d4906f02d195d78fead7f2d9cd
anast <anastasia@borodulina.me>
2025-01-27 00:08:36 +0700

    э

--- level.py
+++ level.py
@@ -46,32 +46,31 @@ class Level:
 
 
 class CameraGroup(pygame.sprite.Group):
-    class CameraGroup(pygame.sprite.Group):
-        def __init__(self):
-            super().__init__()
-            self.display_surface = pygame.display.get_surface()
-            self.offset = pygame.math.Vector2()
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.offset = pygame.math.Vector2()
 
-            # Загрузка карты один раз
-            self.map_image = pytmx.util_pygame.load_pygame('./things/graphics/map.tmx')
+        # Загрузка карты один раз
+        self.map_image = pytmx.util_pygame.load_pygame('./things/graphics/map.tmx')
 
-        def custom_draw(self, player):
-            map_width = self.map_image.width * self.map_image.tilewidth
-            map_height = self.map_image.height * self.map_image.tileheight
+    def custom_draw(self, player):
+        map_width = self.map_image.width * self.map_image.tilewidth
+        map_height = self.map_image.height * self.map_image.tileheight
 
-            self.offset.x = player.rect.centerx - screen_width // 2
-            self.offset.y = player.rect.centery - screen_height // 2
+        self.offset.x = player.rect.centerx - screen_width // 2
+        self.offset.y = player.rect.centery - screen_height // 2
 
-            self.offset.x = max(0, min(self.offset.x, map_width - screen_width))
-            self.offset.y = max(0, min(self.offset.y, map_height - screen_height))
+        self.offset.x = max(0, min(self.offset.x, map_width - screen_width))
+        self.offset.y = max(0, min(self.offset.y, map_height - screen_height))
 
-            # Отрисовка спрайтов с учетом сдвига камеры
-            for layer in layers.values():
-                for sprite in self.sprites():
-                    if sprite.z == layer:
-                        offset_rect = sprite.rect.copy()
-                        offset_rect.center -= self.offset
-                        self.display_surface.blit(sprite.image, offset_rect)
+        # Отрисовка спрайтов с учетом сдвига камеры
+        for layer in layers.values():
+            for sprite in self.sprites():
+                if sprite.z == layer:
+                    offset_rect = sprite.rect.copy()
+                    offset_rect.center -= self.offset
+                    self.display_surface.blit(sprite.image, offset_rect)
 
 
 class Tile(pygame.sprite.Sprite):
